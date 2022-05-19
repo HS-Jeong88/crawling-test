7923cb01d283b5e353b7820368f50e61a2adcd31
HS-Jeong88 <68678298+HS-Jeong88@users.noreply.github.com>
2022-05-19 17:35:44 +0900

    deploy to gh-pages

---
+++ _next/static/sEsKAcnvUUkchwS_tsnU2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/112-86630b6ff7d9cc22.js","static/chunks/pages/index-6abb4c12b329e26a.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
